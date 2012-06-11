d89a75f6d133829d2e1d62531b3e2420f6828f35
Evandro Saroka <saroka@gmail.com>
2012-06-10 22:38:45 -0300

    testing initialization and log levels to appropriate devices

--- spec/x9_logger_spec.rb
+++ spec/x9_logger_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe X9Logger do
-  it 'does something'
+  subject {
+    X9Logger.new do |l|
+      l.out = 'out.log'
+      l.err = 'err.log'
+    end
+  }
+
+  after :each do
+    FileUtils.rm 'out.log'
+    FileUtils.rm 'err.log'
+  end
+
+  it 'receives a block with configuration of out and err' do
+    subject.out.should_not be_nil
+    subject.err.should_not be_nil
+  end
+
+  %w(debug info warn).each do |level|
+    it "logs #{level} level to out device" do
+      subject.send level, "#{level} level"
+      File.read('out.log').should include("#{level} level")
+    end
+  end
+
+  %w(error fatal).each do |level|
+    it "logs #{level} level to err device" do
+      subject.send level, "#{level} level"
+      File.read('err.log').should include("#{level} level")
+    end
+  end
 end
